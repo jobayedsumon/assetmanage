d776181e6599436575b5ce3e43b32382bdab1fb6
Jobayed Sumon <jobayedahmedsumon@gmail.com>
2021-01-30 16:42:40 +0600

    Project Initialized with Material Preset

---
+++ resources/views/layouts/footers/auth.blade.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+  <div class="container-fluid">
+    <nav class="float-left">
+
+    </nav>
+    <div class="copyright float-right">
+      &copy;
+      <script>
+        document.write(new Date().getFullYear())
+      </script>, made with <i class="material-icons">favorite</i> by
+      <a href="#" target="_blank">MIS Department</a> for a better assets and HR management.
+    </div>
+  </div>
+</footer>
